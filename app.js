7a6d31029dc02e5f76a9a393b889bb5c7b324f3c
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-07-16 22:42:30 +0530

    adding main functions

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-console.log("Hello Shivam");
+function getHistory () {
+    return document.getElementById("history-value").innerText;
+}
+
+function printHisory (num) {
+    document.getElementById("history-value").innerText = num;
+}
+
+function getOutput () {
+    return document.getElementById("output-value").innerText;
+}
+
+
+function printOutput (num){
+    if(num=="") {
+        document.getElementById("output-value").innerText=num;
+    } else {
+        document.getElementById("output-value").innerText = getFormattedNumber(num);
+    }
+}
+
+function getFormattedNumber (num) {
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+
+function reverseNumberFormat(num) {
+    return Number(num.replace(/,/g,''));
+}
+
+var operator =  document.getElementsByClassName("operator");
+for(var i = 0; i < operator.length; i++) {
+    operator[i].addEventListener('click', function() {
+        
+    });
+};
+
+var number =  document.getElementsByClassName("number");
+for(var i = 0; i < number.length; i++) {
+    number[i].addEventListener('click', function() {
+        
+    });
+};
